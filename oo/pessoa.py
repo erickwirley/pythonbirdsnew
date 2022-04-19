4978347a102b1709ce8005bc19dd6688679c715f
Erick Magalhaes <erickwirley@gmail.com>
2022-04-19 10:13:33 -0300

    atributo de dados

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,18 @@
 class Pessoa:
-    pass
+    def __init__(self, name= None, idade= 0):
+        self.idade = idade
+        self.nome = name
+
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('teste')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Erick'
+    print(p.nome)
+    print(p.idade)
